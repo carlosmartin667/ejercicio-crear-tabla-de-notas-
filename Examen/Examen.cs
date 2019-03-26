a1b2d3629188be86045605ba9ceb905b5229ca4a
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-03-26 11:22:00 -0300

    invertir palabra

--- Examen/Examen.cs
+++ Examen/Examen.cs
@@ -12,30 +12,52 @@ namespace Examen
     {
       Console.WriteLine("Primer examen");
       //generando los string
-    
-      string[] stringArray = new string[100];   
 
-      string[] vocales = new string[5] { "a", "e", "i", "o", "u" };
-      string[] vocalesMayuscula = new string[5] { "A", "E", "I", "O", "U" };
-      string[] consonantes = new string[21]
-      { "b", "c", "d",  "f", "g", "h", "i", "j", "k"
-      , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t", "v", "w" , "y", "z"
-      };
-      string[] consonantesMayuscula = new string[21]
-     { "B", "C", "D", "f", "G", "H", "I", "J", "K"
+      string[] stringArray = new string[100];
+
+      string[] letras = new string[52] { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U",
+        "b", "c", "d",  "f", "g", "h", "i", "j", "k"
+      , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t", "v", "w" , "y", "z",
+        "B", "C", "D", "f", "G", "H", "I", "J", "K"
       , "L" , "M" , "N" , "P" , "Q" , "R" , "S" , "T", "V", "W" , "Y", "Z"
-     };
+      };
 
       Random random = new Random();
       string randomString = " ";
+      //var palabras = new List<int>();
+      //while (palabras.Count < 1000)
+      //{
+      //  int Number = new Random().Next(1, 26);
 
-      for (int i = 0; i < 10; i++)
+      //  for (int j = 0; j < Number; j++)
+      //  {
+      //    randomString += letras[random.Next(1, 52)].ToString();
+      //  }
+      //  //Console.WriteLine(randomString);
+
+      //  Console.WriteLine();
+      //}
+
+      //foreach (int palabra in palabras)
+      //  Console.Write("{0} ", palabras);
+      //Console.WriteLine();
+      int randomNumber = random.Next(1, 26);
+
+      for (int j = 0; j < randomNumber; j++)
       {
-        randomString += consonantes[random.Next(1, 20)].ToString();
+        randomString += letras[random.Next(1, 52)].ToString();
       }
-
       Console.WriteLine(randomString);
 
+      Console.WriteLine();
+
+      //invertir palabra 
+      string PalabraInvertida = " ";
+      for (int i = randomString.Length -1; i >= 0; i--)
+      {
+        PalabraInvertida += randomString[i];
+      }
+      Console.WriteLine(PalabraInvertida);
     }
   }
 }
