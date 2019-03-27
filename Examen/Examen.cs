79dff7053e40d6a42c86de1e0b32483a059c87fa
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-03-27 12:06:12 -0300

    resolviendo error

--- Examen/Examen.cs
+++ Examen/Examen.cs
@@ -22,47 +22,41 @@ namespace Examen
       , "L" , "M" , "N" , "P" , "Q" , "R" , "S" , "T", "V", "W" , "Y", "Z"
       };
 
-      Random random = new Random();
-      string randomString = " ";
-      //var palabras = new List<int>();
-      //while (palabras.Count < 1000)
-      //{
-      //  int Number = new Random().Next(1, 26);
-
-      //  for (int j = 0; j < Number; j++)
-      //  {
-      //    randomString += letras[random.Next(1, 52)].ToString();
-      //  }
-      //  //Console.WriteLine(randomString);
-
-      //  Console.WriteLine();
-      //}
-
-      //foreach (int palabra in palabras)
-      //  Console.Write("{0} ", palabras);
-      //Console.WriteLine();
-
-      int randomNumber = random.Next(1, 26);
-      //for (int i = 0; i < 100; i++)
-      //{
-      //  for (int j = 0; j < randomNumber; j++)
-      //  {
-      //    randomString += letras[random.Next(1, 52)].ToString();
-      //    i++;
-      //  }
-      //  Console.WriteLine(randomString);
-      //}
-      Console.WriteLine(randomString);
-      for (int j = 0; j < randomNumber; j++)
+      string[] letrasMinusculas = new string[26]
       {
-        randomString += letras[random.Next(1, 52)].ToString();
-    
-      }
-      Console.WriteLine("palabra generada aleatoriamente ");
-      //stringArray + randomString;
-      Console.WriteLine(randomString);
+        "a", "e", "i", "o", "u", "b", "c", "d",  "f", "g", "h", "i", "j", "k"
+      , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t", "v", "w" , "y", "z"
+      };
+      string[] letrasMayuscula = new string[26]
+      {
+        "A", "E", "I", "O", "U","B", "C", "D", "f", "G", "H", "I", "J", "K"
+      , "L" , "M" , "N" , "P" , "Q" , "R" , "S" , "T", "V", "W" , "Y", "Z"
+      };
 
-      Console.WriteLine();
+      Random random = new Random();
+      string randomString = "    ";
+      var palabras = new List<int>();
+      int num = 0; 
+      while (num < 100)
+      {
+        int Number = new Random().Next(1, 20);
+
+        for (int j = 0; j < Number; j++)
+        {
+          randomString = letras[random.Next(0, 52)].ToString();
+          //stringArray[j] = letras[random.Next(0, 52)].ToString();
+          for (int i = 0; i < 1; i++)
+          {
+
+            Console.Write(randomString);
+          }
+        }
+        num++;
+        Console.WriteLine();
+      }
+
+      int randomNumber = random.Next(1, 21);
+     
 
       //invertir palabra 
       string PalabraInvertida = " ";
