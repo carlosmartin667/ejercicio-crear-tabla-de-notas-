9dc972f13b33756e77302184277513b8395c7cf2
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-03-28 16:08:12 -0300

    version 2 cajero

--- Cajero2/Program.cs
+++ Cajero2/Program.cs
@@ -18,79 +18,44 @@ namespace Exmen2
 
       Dinero = float.Parse(linea);
       Console.WriteLine("La Cantidad introducida es: " + Dinero);
+      for (int i = 0; i < 7; i++)
+      {
+        if ((Dinero >= BILLETESVALOR[i]))
+        {
+          BILLETES[i] = ((int)Dinero / BILLETESVALOR[i]);
+          Dinero = Dinero - (BILLETES[i] * BILLETESVALOR[i]);
+        }
+      }
 
-     // BILLETES CALCULO
-      if ((Dinero >= BILLETESVALOR[0]))
-      {
-        BILLETES[0] = ((int)Dinero / BILLETESVALOR[0]);
-        Dinero = Dinero - (BILLETES[0] * BILLETESVALOR[0]);
-      }
-      if ((Dinero >= BILLETESVALOR[1]))
-      {
-        BILLETES[1] = ((int)Dinero / BILLETESVALOR[1]);
-        Dinero = Dinero - (BILLETES[1] * BILLETESVALOR[2]);
-      }
-      if ((Dinero >= BILLETESVALOR[2]))
-      {
-        BILLETES[2] = ((int)Dinero / BILLETESVALOR[2]);
-        Dinero = Dinero - (BILLETES[2] * BILLETESVALOR[2]);
-      }
-      if ((Dinero >= BILLETESVALOR[3]))
-      {
-        BILLETES[3] = ((int)Dinero / BILLETESVALOR[3]);
-        Dinero = Dinero - (BILLETES[3] * BILLETESVALOR[3]);
-      }
-      if ((Dinero >= BILLETESVALOR[4]))
-      {
-        BILLETES[4] = ((int)Dinero / BILLETESVALOR[4]);
-        Dinero = Dinero - (BILLETES[4] * BILLETESVALOR[4]);
-      }
-      if ((Dinero >= BILLETESVALOR[5]))
-      {
-        BILLETES[5] = ((int)Dinero /  BILLETESVALOR[5]);
-        Dinero = Dinero - (BILLETES[5] * BILLETESVALOR[5]);
-      }
-      if ((Dinero >= BILLETESVALOR[6]))
-      {
-        BILLETES[6] = ((int)Dinero / BILLETESVALOR[6]);
-        Dinero = Dinero - (BILLETES[6] * BILLETESVALOR[6]);
-      }
       //MONEDAS CALCULO
-      if ((Dinero >= MONEDASVALOR[0]))
+
+      for (int i = 0; i < 5; i++)
       {
-        MONEDAS[0] = (int)(Dinero / MONEDASVALOR[0]);
-        Dinero = Dinero - (float)(MONEDAS[0] * MONEDASVALOR[0]);
+        if ((Dinero >= MONEDASVALOR[i]))
+        {
+          MONEDAS[i] = (int)(Dinero / MONEDASVALOR[i]);
+          Dinero = Dinero - (float)(MONEDAS[i] * MONEDASVALOR[i]);
+        }
       }
 
-      if ((Dinero >= MONEDASVALOR[1]))
+      for (int i = 0; i < 7; i++)
       {
-        MONEDAS[1] = (int)(Dinero / MONEDASVALOR[1]);
-        Dinero = Dinero - (float)(MONEDAS[1]* MONEDASVALOR[1]);
+        Console.WriteLine("Billetes de {i }: " + BILLETES[i]);
+
       }
-      if ((Dinero >= MONEDASVALOR[2]))
+      //Console.WriteLine("Billetes de 500: " + BILLETES[0]);
+      //Console.WriteLine("Billetes de 200: " + BILLETES[1]);
+      //Console.WriteLine("Billetes de 100: " + BILLETES[2]);
+      //Console.WriteLine("Billetes de 50 : " + BILLETES[3]);
+      //Console.WriteLine("Billetes de 20 : " + BILLETES[4]);
+      //Console.WriteLine("Billetes de 10 : " + BILLETES[5]);
+      //Console.WriteLine("Billetes de 5 : "  + BILLETES[6]);
+
+      for (int i = 0; i < 5; i++)
       {
-        MONEDAS[2] = (int)(Dinero / MONEDASVALOR[2]);
-        Dinero = Dinero - (float)(MONEDAS[2] * MONEDASVALOR[2]);
+        Console.WriteLine("Monedas de  : " + MONEDAS[i]);
       }
 
-      if ((Dinero >= MONEDASVALOR[3]))
-      {
-        MONEDAS[3] = (int)(Dinero / MONEDASVALOR[3]);
-        Dinero = Dinero - (float)(MONEDAS[3] * MONEDASVALOR[3]);
-      }
-      if ((Dinero >= MONEDASVALOR[4]))
-      {
-        MONEDAS[4] = (int)Math.Round(Dinero / MONEDASVALOR[4]);
-        Dinero = Dinero - (float)(MONEDAS[4] * MONEDASVALOR[4]);
-      }
-
-      Console.WriteLine("Billetes de 500: " + BILLETES[0]);
-      Console.WriteLine("Billetes de 200: " + BILLETES[1]);
-      Console.WriteLine("Billetes de 100: " + BILLETES[2]);
-      Console.WriteLine("Billetes de 50 : " + BILLETES[3]);
-      Console.WriteLine("Billetes de 20 : " + BILLETES[4]);
-      Console.WriteLine("Billetes de 10 : " + BILLETES[5]);
-      Console.WriteLine("Billetes de 5 : "  + BILLETES[6]);
 
       Console.WriteLine("Monedas de 0,50 : " + MONEDAS[0]);
       Console.WriteLine("Monedas de 0,25 : " + MONEDAS[1]);
